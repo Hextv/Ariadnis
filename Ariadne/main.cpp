ccc7f82f1d69509b7c2ec12be4e53852298421f7
blairess <vancho.mladenov@gmail.com>
2026-07-23 18:44:08 +0300

    Part 1/2 of Rendering a triangle

--- Ariadne/main.cpp
+++ Ariadne/main.cpp
@@ -1,3 +1,7 @@
+// Personal Comment
+
+// Need to finish rendering a triangle / Current Page 36
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -10,8 +14,8 @@ void processInput(GLFWwindow* window)
 }
 
 // Main Function
-int main() 
-	{
+int main()
+{
 	// GLFW Initialize
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -37,6 +41,53 @@ int main()
 
 	// Viewport
 	glViewport(0, 0, 800, 600);
+
+	// Vertex Shader
+	const char* vertexShaderSource = "#version 330 core \n"
+		"layout (location = 0) in vec3 aPos; \n"
+		"void main() \n"
+		"{ \n"
+		" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
+		"} \0";
+
+	// Create shader object and shader source code
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Check if compilation was successful
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR: SHADER / VERTEX / COMPILATION_FAILED \n" << infoLog << std::endl;
+	}
+
+	// Define Verticles [For Triangle]
+	float vertices[]{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// Vetex Buffer Objects
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);												// Select VBO as the active vertex buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		// Copy vertex data into the GPU's memory
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Define how OpenGL should read our vertex data 
+	// (Location 0, 3 floats per position, stride of 3 floats, starting at index 0)
+
+	glEnableVertexAttribArray(0); // Turn on location 0 so the GPU can use this data
+
 
 	// Render Loop
 	while (!glfwWindowShouldClose(window))
