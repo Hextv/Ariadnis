57503ae179bb17afbac7b6e797671324820c7c08
blairess <vancho.mladenov@gmail.com>
2026-07-25 09:07:43 +0300

    Terrain Skeleton

--- src/terrain/terrain.h
+++ src/terrain/terrain.h
@@ -11,5 +11,22 @@ public:
 	float cellSize; // Size of a single square
 
 	std::vector<float> vertices;
-	std::vector<float> indices;
+	std::vector<int> indices;
+
+	unsigned int VAO, VBO, EBO; // OpenGL buffers
+
+	// Controls the Mesh generation
+	void generateMesh() {
+		// Walk through each grid point on the X and Z axes to build the terrain mesh
+		for (int z = 0; z < depth; z++) {
+			for (int x = 0; x < width; x++) {
+				// Code
+			}
+		}
+	}
+
+	// Future send information toward the GPU
+	void setupBuffers() {
+
+	}
 };
