a44ff3ee5e70f34018d0ece3393e082f4850d04a
blairess <vancho.mladenov@gmail.com>
2026-07-25 15:52:30 +0300

    Camera Setup

---
+++ src/core/camera/camera.cpp
@@ -0,0 +1,5 @@
+#include "camera.h"
+
+void Camera::processKeyboard(CameraMovement direction, float deltaTime) {
+	float velocity = MovementSpeed * deltaTime;
+}
--- src/core/camera/camera.h
+++ src/core/camera/camera.h
@@ -2,9 +2,9 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 
-#pragma once // Prevents the header file from being included multiple times
+#pragma once
 
-enum class CameraDirection {
+enum class CameraMovement {
 	NONE = 0,
 	FORWARD,
 	BACKWARD,
@@ -16,11 +16,21 @@ enum class CameraDirection {
 
 class Camera {
 public:
-	// Vectors
+	// Camera Vectors
 	glm::vec3 CameraPos;
-
-	// TODO: Need to initilize values.
 	glm::vec3 CameraFront;
 	glm::vec3 CemeraUp;
 	glm::vec3 CameraRight;
+	glm::vec3 WorldUp;
+
+	// Angles
+	float Yaw;			// Left and Right
+	float Pitch;	   // Up and Down 
+
+	// Options
+	float MouseSesitivity;
+	float MovementSpeed;
+	float Zoom;
+
+	void processKeyboard(CameraMovement direction, float deltaTime);
 };
